084ee50c3dc4d5511bb7a3ac55c41d37f5028b9a
Igor Souza <igornevesdesouza@gmail.com>
2025-09-26 23:31:26 -0300

    Ajuste da rota inicial para index

--- gerenciador/urls.py
+++ gerenciador/urls.py
@@ -1,11 +1,12 @@
 # gerenciador/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('projetos/', include(('projetos.urls', 'projetos'), namespace='projetos')),
     path('tarefas/', include(('tarefas.urls', 'tarefas'), namespace='tarefas')),
-    path('', include(('projetos.urls', 'projetos'), namespace='root')),  # página inicial -> projetos
+    path('', index, name='index'),
 ]
