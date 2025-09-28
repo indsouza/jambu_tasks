591c30ad2a40b26f0e3e6334c7dfd27682c2b56b
Igor Souza <igornevesdesouza@gmail.com>
2025-09-28 10:52:50 -0300

    Ajustes das rotas e templates de Tarefas

--- projetos/forms.py
+++ projetos/forms.py
@@ -5,4 +5,9 @@ class FormularioProjeto(forms.ModelForm):
     class Meta:
         model = Projeto
         fields = ['titulo', 'descricao', 'participantes']
+        widgets = {
+            "titulo": forms.TextInput(attrs={"class": "form-control"}),
+            "descricao": forms.Textarea(attrs={"class": "form-control", "rows": 2}),
+            "participantes": forms.SelectMultiple(attrs={"class": "form-control"}),
+        }
 
--- tarefas/forms.py
+++ tarefas/forms.py
@@ -4,8 +4,12 @@ from .models import Tarefa
 class FormularioTarefa(forms.ModelForm):
     class Meta:
         model = Tarefa
-        fields = ['nome', 'detalhes', 'projeto', 'responsavel', 'prazo', 'status']
+        fields = ["nome", "detalhes", "projeto", "responsavel", "status", "prazo"]
         widgets = {
-            'prazo': forms.DateInput(attrs={'type': 'date'}),
+            "nome": forms.TextInput(attrs={"class": "form-control"}),
+            "detalhes": forms.Textarea(attrs={"class": "form-control", "rows": 2}),
+            "projeto": forms.Select(attrs={"class": "form-control"}),
+            "responsavel": forms.Select(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
+            "prazo": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
         }
-
